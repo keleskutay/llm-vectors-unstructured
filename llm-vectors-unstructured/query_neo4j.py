452b2c4ce6d4cdb4f4bd1da2ed1978584780f750
Kutay <kutaykeless@gmail.com>
2026-03-19 17:03:33 +0300

    query generated embedding

--- llm-vectors-unstructured/query_neo4j.py
+++ llm-vectors-unstructured/query_neo4j.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from openai import OpenAI
+#from neo4j import GraphDatabase
+from langchain_neo4j import Neo4jGraph
 
 llm = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
@@ -14,10 +16,20 @@ response = llm.embeddings.create(
 embedding = response.data[0].embedding
 
 # Connect to Neo4j
-# graph = 
+#graph = GraphDatabase.driver(uri=os.getenv("NEO4J_URI"),
+#                            auth=(os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD")),
+#                           database=os.getenv("NEO4J_DATABASE")
+#
+#                              ) 
+graph = Neo4jGraph(url=os.getenv("NEO4J_URI"), 
+                   username=os.getenv("NEO4J_USERNAME"), 
+                   password=os.getenv("NEO4J_PASSWORD"), 
+                   database=os.getenv("NEO4J_DATABASE"))
 
 # Run query
-# result = 
+result = graph.query(f"CALL db.index.vector.queryNodes('chunkVector', 6, {embedding}) YIELD node, score RETURN node.text, score")
 
 # Display results
-# for row ... 
+for row in result:
+    print(row)
+   
